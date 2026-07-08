f09c8be4b8cc6ef67f881aa76e5c3f08e530e0d8
Abdul Ahad <abdulahad010274@gmail.com>
2026-07-08 13:43:43 +0500

    hghf

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import apiRoutes from './src/routes/apiRoutes.js';
-import { errorHandler } from './src/middlewares/errorHandler.js';
 
 dotenv.config();
 
@@ -24,31 +23,12 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', apiRoutes);
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
 
-process.on('uncaughtException', (error) => {
-  console.error('UNCAUGHT EXCEPTION! Shutting down...', error);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('UNHANDLED REJECTION! Shutting down...', reason);
-  server.close(() => {
-    process.exit(1);
-  });
-});
 
 const gracefulShutdown = (signal) => {
   console.log(`Received ${signal}. Shutting down gracefully...`);
