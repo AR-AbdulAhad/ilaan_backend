77b37aeb8b04a1fcdd11a7cd09580e9a9df40d02
Abdul Ahad <abdulahad010274@gmail.com>
2026-07-13 20:41:57 +0500

    commit

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,3 +1,76 @@
+// import { Router } from 'express';
+// import {
+//   getProducts,
+//   getProductById,
+//   createProduct,
+//   updateProduct,
+//   deleteProduct,
+//   uploadImages,
+//   bulkImport,
+//   bulkExport,
+//   getMetadata,
+//   getFilters,
+//   getCategories,
+//   createCategory,
+//   updateCategory,
+//   deleteCategory,
+//   getSubcategories,
+//   createSubcategory,
+//   updateSubcategory,
+//   deleteSubcategory,
+//   getScreenTypes,
+//   createScreenType,
+//   updateScreenType,
+//   deleteScreenType
+// } from '../controllers/productController.js';
+// import upload from '../middlewares/upload.js';
+
+// const router = Router();
+
+// // Products search, lisgetProductst, create
+// router.get('/', getProducts);
+// router.post('/', createProduct);
+
+// // Dynamic filters
+// router.get('/filters', getFilters);
+
+// // Dynamic categories & subcategories metadata
+// router.get('/metadata', getMetadata);
+
+// // Categories CRUD
+// router.get('/categories', getCategories);
+// router.post('/categories', createCategory);
+// router.put('/categories/:id', updateCategory);
+// router.delete('/categories/:id', deleteCategory);
+
+// // Subcategories CRUD
+// router.get('/subcategories', getSubcategories);
+// router.post('/subcategories', createSubcategory);
+// router.put('/subcategories/:id', updateSubcategory);
+// router.delete('/subcategories/:id', deleteSubcategory);
+
+// // ScreenTypes CRUD
+// router.get('/screentypes', getScreenTypes);
+// router.post('/screentypes', createScreenType);
+// router.put('/screentypes/:id', updateScreenType);
+// router.delete('/screentypes/:id', deleteScreenType);
+
+// // Bulk import & export
+// router.post('/import', upload.single('file'), bulkImport);
+// router.get('/export', bulkExport);
+
+// // Multiple image upload endpoint
+// router.post('/upload-images', upload.array('images', 10), uploadImages);
+
+// // Get, update, delete single product
+// router.get('/:idOrSlug', getProductById);
+// router.put('/:id', updateProduct);
+// router.delete('/:id', deleteProduct);
+
+// export default router;
+
+
+
 import { Router } from 'express';
 import {
   getProducts,
@@ -21,7 +94,8 @@ import {
   getScreenTypes,
   createScreenType,
   updateScreenType,
-  deleteScreenType
+  deleteScreenType,
+  backfillPriceGbp
 } from '../controllers/productController.js';
 import upload from '../middlewares/upload.js';
 
@@ -62,10 +136,12 @@ router.get('/export', bulkExport);
 // Multiple image upload endpoint
 router.post('/upload-images', upload.array('images', 10), uploadImages);
 
+// Backfill priceGbp for existing products imported with only landedCostGbp
+router.post('/backfill-prices', backfillPriceGbp);
+
 // Get, update, delete single product
 router.get('/:idOrSlug', getProductById);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
 export default router;
-
